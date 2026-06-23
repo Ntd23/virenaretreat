7d2f2676dc8d1e81a7350c852806c8ce98a55026
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2026-06-23 10:25:50 +0700

    feat: modernize authentication and user dashboard UI with new custom styles and font integration

--- modules/Layout/app.blade.php
+++ modules/Layout/app.blade.php
@@ -29,6 +29,7 @@
     <link href="{{ asset('libs/select2/css/select2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('dist/frontend/css/notification.css') }}" rel="newest stylesheet">
     <link href="{{ asset('dist/frontend/css/app.css?_ver='.config('app.asset_version')) }}" rel="stylesheet">
+    <link href="{{ asset('dist/frontend/css/custom-modern-user.css?_ver='.config('app.asset_version')) }}" rel="stylesheet">
 
     <link rel="stylesheet" type="text/css" href="{{ asset("libs/daterange/daterangepicker.css") }}" >
     <!-- Fonts -->
